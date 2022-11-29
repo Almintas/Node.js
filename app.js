969508e40d5ed2e08755e80028bf2ba0a9a55772
Almintas <almintasbruzas@gmail.com>
2022-11-29 21:27:14 +0200

    Lesson-1

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type' : 'application/json'
-    });
+const CARS = [
+    { brand: 'BMW', model: '3', year: '2010', color: 'white' },
+    { brand: 'AUDI', models: 'A6', year: '2015', clor: 'black' }
+];
 
-    const response = {
-        email: 'test@gmail.com',
-        name: 'Petras',
-        surname: 'Petraitis'
-    }
-    res.write(JSON.stringify(response));
-    res.end();
+const USERS = [
+    { id: 1, name: 'Petras', surname: 'Petraitis' },
+    { id: 2, name: 'Martynas', surname: 'Martinaitis' }
+];
+
+app.get('/cars', (req, res) => {
+   res.send(CARS);
 });
 
-server.listen(PORT, () => console.log(`${PORT}`));
+app.get('/api/cars/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    const car = CARS.find((car) => car.id === id);
+
+    if (!car) {
+        res.status(400).send('Car was not found')
+    }
+
+    res.send(car);
+
+})
+
+app.get('/api/users', (req, res) => {
+res.send(USERS)
+});
+
+app.get('/api/users/:firstLetter', (res, req) => {
+const firstLetter = req.params.firstLetter.toLowerCase();
+const filteredUsers = USERS.filter((user) => user.name.toLowerCase().indexOf(firstLetter) === 0)
+res.send(filteredUsers)
+});
+
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
