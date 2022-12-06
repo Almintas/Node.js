fd08f199f656dcb8641fa9bd8d885233789b6c38
Almintas <almintasbruzas@gmail.com>
2022-12-06 18:06:47 +0200

    Lesson-4

--- app.js
+++ app.js
@@ -1,54 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const cors = require('cors');
+
+require('dotenv').config({ path: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev' });
 
 app.use(cors());
-app.use(express.json());
 
-let TASKS = [
-    { id: 1, title: 'isplauti indus', isDone: false },
-    { id: 2, title: 'issiurbti namus', isDone: false },
-    { id: 3, title: 'pasimokyti programuoti', isDone: true },
-];
+console.log(process.env.API_URL);
 
-app.get('/api/tasks', (req, res) => {
-res.send(TASKS)
+app.get('/name', (req, res) => {
+    res.send('My name is Petras');
 });
 
-app.get('/api/tasks/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const tasks = TASKS.find((task) => task.id === id);
-    res.send(tasks);
-});
-
-app.post('/api/tasks', (req, res) => {
-const lastTask = TASKS[TASKS.length -1];
-const newTask = { ...req.body, id: lastTask.id +1 ,isDone: false };
-
-TASKS.push(newTask);
-
-res.send(TASKS);
-});
-
-app.put('/api/tasks/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const newTask = req.body;
-
-    const updatedTasks = TASKS.map((task) => {
-        if (task.id === id) {
-            return {
-                ...newTask,
-                id: task.id
-            };
-        }
-        return task;
-    });
-
-    TASKS = [...updatedTasks];
-
-    res.send(TASKS);
-
-})
-
-app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+app.listen(3000, () => console.log('server is online'));
