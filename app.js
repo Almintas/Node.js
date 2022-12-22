6cd240466882ddbc297e8b8965b6b5648c9157c6
Almintas <almintasbruzas@gmail.com>
2022-12-22 20:16:23 +0200

    Lesson-11

--- app.js
+++ app.js
@@ -1,154 +1,85 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
-
-require('dotenv').config({ path: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev' });
+const mysql = require('mysql2');
+const { response } = require('express');
 
 app.use(cors());
 app.use(express.json());
 
-const mongoDB = 'mongodb+srv://Almintas:kikitata@cluster0.zc6jd3y.mongodb.net/cars-portal?retryWrites=true&w=majority';
+const mysqlConfig = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Kikitata147',
+    database: 'shop',
+    port: 3306
+};
 
-mongoose.connect(mongoDB);
+const connection = mysql.createConnection(mysqlConfig);
 
-const db = mongoose.connection;
-
-db.on('error', (error) => console.log(error));
-db.once('open', () => console.log('Connected to MongoDB!'));
-
-const carSchema = new mongoose.Schema({
-    brand: {
-        type: String,
-        required: true
-    },
-    model: {
-        type: String,
-        required: true
-    },
-    year: {
-        type: Number,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    }
-});
-const carModel = mongoose.model('car', carSchema);
-
-const carUsers = new mongoose.Schema({
-    user: {
-        type: String,
-        required: true
-    },
-    brand: {
-        type: String,
-        required: true
-    }
-});
-
-const userAdverts = new mongoose.Schema({
-    brand: {
-        type: String,
-        reqiured: true
-    },
-    model: {
-        type: String,
-        reqiured: true
-    },
-    price: {
-        type: Number,
-        reqiured: true
-    },
-    user_id: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    }
-});
-
-const carUserAdverts = mongoose.model('adverts', userAdverts);
-
-const carModelUsers = mongoose.model('users', carUsers);
-
-
-console.log(process.env.API_URL);
-
-app.get('/cars', async (req, res) => {
-    const data = await carModel.find().sort({ year: 1 });
-    res.send(data);
-});
-
-app.get('/cars/:brand', async (req, res) => {
-    const cars = await carModel.find({
-        brand:
-        {
-            $regex:
-                new RegExp(req.params.brand, 'i')
-        }
+app.get('/products', (req, res) => {
+    connection.execute('SELECT * FROM products', (err, products) => {
+        res.send(products)
     });
-    // const cars = await carModel.find({ brand });
-    res.send(cars);
 });
 
-app.get('/users/asc', async (req, res) => {
-    const users = await carModelUsers.find().sort({ user: 1 });
-    res.send(users);
+app.post('/products', (req, res) => {
+    const { title, price, stock } = req.body;
+
+    connection.execute('INSERT INTO products (title, price, stock) VALUES(?, ?, ?)', [title, price, stock], (err, products) => {
+        connection.execute('SELECT * FROM products', (err, products) => {
+            res.send(products)
+        });
+    });
 });
 
-app.post('/cars', async (req, res) => {
-    const { brand, model, year, price } = req.body;
-    await carModel.create({ brand, model, year, price });
-    const cars = await carModel.find();
-    res.send(cars);
+app.patch('/products/:id', (req, res) => {
+    const { title, price, stock } = req.body;
+
+    connection.execute('UPDATE products SET title=?, price=?, stock=?, WHERE id=?', [title, price, stock, req.params.id], (err, result) => {
+        connection.execute('SELECT * FROM products', (err, result) => {
+            res.send(result);
+        });
+    });
 });
 
-app.get('/users', async (req, res) => {
-    const data = await carModelUsers.find();
-    res.send(data);
+app.delete('/products/:id', (req, res) => {
+    connection.execute('DELETE FROM products WHERE id=?', [req.params.id], (err, result) => {
+        connection.execute('SELECT * FROM products', (err, result) => {
+            res.send(result);
+        });
+    });
 });
 
-app.post('/users', async (req, res) => {
-    const { user, brand } = req.body;
-    await carModelUsers.create({ user, brand });
-    const users = await carModelUsers.find();
-    res.send(users);
+app.get('/sales', (req, res) => {
+    connection.execute(
+        `SELECT customers.id as customerid, sales.id as saleid, name amount as saleamount
+        FROM sales 
+        INNER JOIN customers 
+        ON customers.id=sales.customerid;`,
+        (err, result) => {
+            connection.execute('SELECT * FROM sales', (err, result) => {
+                res.send(result);
+            });
+        });
 });
 
-app.get('/users/:name', async (req, res) => {
-    const { name } = req.params;
-    const users = await carModelUsers.find({ name });
-    res.send(users);
-});
+// app.post('/employees', (req, res) => {
+//     connection.execute('INSERT INTO employees (name, salary) VALUES (?, ?)', [req.body.name, req.body.salary], (err, result) => {
+//         connection.execute('SELECT * FROM employees', (err, result) =>{
+//             res.send(result);
+//         });
+//     });
+// });
 
-app.delete('/adverts/:id', async (req, res) => {
-    const { id } = req.params;
-    await carUserAdverts.deleteOne({ _id: id });
-    const adverts = await carUserAdverts.find();
-    res.send(adverts);
-});
+// app.patch('/employees/:id', (req,res) => {
+//     const { name } = req.body;
 
-app.post('/adverts', async (req, res) => {
-    const { brand, model, price, description, user_id, } = req.body;
-    await carUserAdverts.create({ brand, model, price, user_id, description });
-    const advertsUser = await carUserAdverts.find();
-    res.send(advertsUser);
-});
-
-app.patch('/adverts/:id', async (req, res) => {
-    const { id } = req.body;
-    // const { brand, model, price } = req.body;
-    await carUserAdverts.updateOne({ _id: id });
-    const data = await carUserAdverts.find();
-    res.send(data);
-});
-
-app.get('/adverts', async (req, res) => {
-    const data = await carUserAdverts.find();
-    res.send(data);
-});
+//     connection.execute('UPDATE employees SET name=? WHERE id=?', [name, req.params.id], (err, result) => {
+//         connection.execute('SELECT * FROM employees', (err, result) => {
+//             res.send(result);
+//         });
+//     });
+// });
 
 app.listen(3000, () => console.log('server is online'));
